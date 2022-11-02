8ef4b03d5a2ac8180a7ca491ad8c1ab0f1ad612f
Rahi <93567798+UppalRahi@users.noreply.github.com>
2022-11-02 11:35:43 +0530

    Create helo.js

---
+++ helo.js
@@ -0,0 +1,11 @@
+function stringPallindrome(string){
+    const length = string.length
+    for(let i = 0; i < length/2 ; i++){
+        if(string[i] !== string[length-i-1])
+        return false
+    }
+
+return true
+}
+
+console.log(stringPallindrome("TetfheT"))
